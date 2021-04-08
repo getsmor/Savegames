94708183683cb4ce1781c0d3e09d5b9ae3b78e3a
Simon Malmstrom <43723030+getsmor@users.noreply.github.com>
2021-04-08 15:55:03 +0200

    Update backup.bat

--- backup.bat
+++ backup.bat
@@ -6,21 +6,28 @@ echo Please specify your backup location with the full address:
 echo (External USB Drive is recommended!)
 set /p setlocation=
 
-set nameCounter=1
-for /f %%x in (gamenames.txt) do (
-  set "nameLine_!nameCounter!=%%x"
-  set /a nameCounter+=1
+
+set NAME="gamenames.txt"
+set /a c=0
+echo Supported Games:
+echo ------------------
+FOR /F "tokens=1 usebackq" %%i in (%NAME%) do (
+    set /a c=c+1
+    echo %%i
 )
 
-set locationCounter=1
-for /f %%x in (gamelocations.txt) do (
-  set "locationLine_!locationCounter!=%%x"
-  set /a locationCounter+=1
-)
+echo ------------------
 
-if exist %locationLine_1% (
-   xcopy %locationLine_1% %setlocation% /E /y
-) else (
-   echo %nameLine_1% Not Found!
+set LOCATION="gamelocations.txt"
+set /a d=0
+FOR /F "tokens=1 usebackq" %%i in (%LOCATION%) do (
+    set /a d=d+1
+    set path=%%i
+    if exist !path! (
+        Xcopy !path! %setlocation% /E /y
+    ) else (
+        echo No files found!
+    )
 )
 pause
+endlocal
